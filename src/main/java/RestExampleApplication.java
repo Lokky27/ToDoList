3207b263c9e333a5f3233a629e9bf1a0ce587eb9
lokky <lokky2789@gmail.com>
2021-07-21 23:40:37 +0300

    initial commit

---
+++ src/main/java/RestExampleApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class RestExampleApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RestExampleApplication.class, args);
+
+    }
+
+}
