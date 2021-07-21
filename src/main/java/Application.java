9f66c430ed7bc9a23726ded46528482129958dce
lokky <lokky2789@gmail.com>
2021-07-21 23:53:23 +0300

    Controller and other classes added

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,12 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class RestExampleApplication
+@SpringBootApplication
+public class Application
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(RestExampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
 
     }
 
